c5b07bc7c026494096999d9faee038dc46005455
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-07-21 14:08:56 -0500

    src/index updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const axios = require('axios')
 const config = require('../config')
 
-// console.log(config.API_KEY)
+console.log(config.API_KEY)
 
 
 // Please note that it is normally not considered best practice to commit 
@@ -12,7 +12,7 @@ const config = require('../config')
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${config.API_KEY}`
   return axios(url)
-  .then(response => response) 
+  .then(response => response)
   .catch(function(error) {
     console.log(error)
   })
@@ -25,6 +25,7 @@ const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${config.API_KEY}`
   return axios(url)
   .then(response => response.data) 
+    console.log(response)
 }
 getMovieById()
 
